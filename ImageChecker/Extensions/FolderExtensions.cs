a4099cc421cd57b33676c69247c0ecf7ea42e95f
Mustafa Ozdemir <mustafa.ozdemir@morhipo.com>
2018-12-28 14:15:41 +0300

    .

---
+++ ImageChecker/Extensions/FolderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ImageChecker.Extensions
+{
+    public static class FolderExtensions
+    {
+        public static string CreateFilePath(string filePath, string fileName, int filePathSize)
+        {
+            var filePaths = Regex.Split(fileName.Substring(0, filePathSize).ToLower(), string.Empty).Where(o => !string.IsNullOrWhiteSpace(o)).ToList();
+            string result = filePaths.Aggregate(string.Empty, (current, item) => current + (item + @"\"));
+            return $@"{filePath}\{result}";
+        }
+    }
+}
