b0130d7414dadce2764699fbcfb8bf00e435db0f
Mustafa Ozdemir <mustafa.ozdemir@morhipo.com>
2018-12-29 17:44:12 +0300

    .

--- Services/Properties/AssemblyInfo.cs
+++ Services/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImageChecker")]
+[assembly: AssemblyTitle("utility-for-directory")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImageChecker")]
+[assembly: AssemblyProduct("utility-for-directory")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
