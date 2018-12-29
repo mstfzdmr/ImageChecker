12828278dffc2ea59df2f52b8bec995a4f77a7b1
Mustafa Ozdemir <mustafa.ozdemir@morhipo.com>
2018-12-29 13:53:30 +0300

    rename

---
+++ ImageChecker/Services/IImageServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImageChecker.Services
+{
+    public interface IImageServices
+    {
+        bool ImageCheck(string imagePath);
+        List<Tuple<string, bool>> ImageCheck(List<string> imagePaths, out List<string> notFoundedUrls);
+    }
+}
