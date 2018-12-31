c9863c2144b4626f742e636fd5de14a2ca53c716
Mustafa Ozdemir <mustafa.ozdemir@morhipo.com>
2018-12-31 16:28:10 +0300

    argument null test

--- Tests/ImageServicesTests.cs
+++ Tests/ImageServicesTests.cs
@@ -1,12 +1,22 @@
-﻿using Xunit;
+﻿using ImageChecker;
+using ImageChecker.Services;
+using System;
+using Xunit;
 
 namespace Tests
 {
     public class ImageServicesTests
     {
-        [Fact]
-        public void ImageCheck__ShouldArgumentNullException_WhenImagePathIsNull()
+        [Theory, InlineData(new object[] { ""})]
+        public void ImageCheck__ShouldArgumentNullException_WhenImagePathIsNull(string imagePath)
         {
+            IImageServices imageServices = new ImageServices();
+            var result = Record.Exception(() => imageServices.ImageCheck(imagePath));
+            Assert.NotNull(result);
+            var exception = Assert.IsType<ArgumentNullException>(result);
+            var actual = exception.ParamName;
+            const string expected = "imagePath";
+            Assert.Equal(expected, actual);
         }
     }
 }
