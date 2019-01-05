eb7409cee72b76a3f6679bdc060cda0b491c6718
Mustafa Ozdemir <mustafa.ozdemir@morhipo.com>
2019-01-05 18:37:42 +0300

    bitmap extension

--- Services/Extensions/FolderExtensions.cs
+++ Services/Extensions/FolderExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using ImageChecker.Helper;
+using System.Drawing;
+using System.Linq;
+using System.Net;
 using System.Text.RegularExpressions;
 
 namespace ImageChecker.Extensions
@@ -10,6 +13,16 @@ namespace ImageChecker.Extensions
             var filePaths = Regex.Split(fileName.Substring(0, filePathSize).ToLower(), string.Empty).Where(o => !string.IsNullOrWhiteSpace(o)).ToList();
             string result = filePaths.Aggregate(string.Empty, (current, item) => current + (item + @"\"));
             return $@"{filePath}\{result}";
+        }
+        public static Bitmap GetBitmap(string fileName, string networkName, NetworkCredential networkCredential)
+        {
+            Bitmap bitmap;
+            using (new NetworkConnection(networkName, networkCredential))
+            {
+                bitmap = new Bitmap(fileName);
+            }
+
+            return bitmap;
         }
     }
 }
